db0c72b39f3ef16ee4ca1cd5f2981be0f1fe9c06
k0r1 <minhquan.k0r1@gmail.com>
2023-08-06 01:30:54 +0700

    Plan config

--- infrastructure/common/variables.tf
+++ infrastructure/common/variables.tf
@@ -3,14 +3,14 @@ variable "cidr_subnet" {
   default = "10.0.0.0/16"
 }
 
-variable "availability_zones" {
-  type    = string
-  default = "us-east-1"
-}
-
 variable "cidr_vpc" {
   type    = string
   default = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type    = string
+  default = "us-east-1"
 }
 
 variable "subnet" {
