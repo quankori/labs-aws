fc4566c885185494bc07e2dcc2eb53fd7921845e
k0r1 <mimnhquan.k0r1@gmail.com>
2023-08-05 12:04:25 +0700

    Update strcture

--- infrastructure/common/main.tf
+++ infrastructure/common/main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0.1"
     }
   }
+  required_version = "~> 1.4.6"
 }
 
 provider "aws" {
