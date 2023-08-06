8136b6df33f7d54eba1253c6670959c6bedf3f78
k0r1 <minhquan.k0r1@gmail.com>
2023-08-06 15:56:24 +0700

    Network

--- infrastructure/common/variables.tf
+++ infrastructure/common/variables.tf
@@ -1,24 +1,34 @@
-variable "cidr_subnet" {
+variable "cidr_public_subnet" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.11.13.0/24"
+}
+
+variable "cidr_private_subnet" {
+  type    = string
+  default = "10.11.12.0/24"
 }
 
 variable "cidr_vpc" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.32.0.0/16"
 }
 
-variable "availability_zones" {
+variable "cidr_route" {
   type    = string
-  default = "us-east-1"
-}
-
-variable "subnet" {
-  type    = string
-  default = "10.0.16.0/24"
+  default = "0.0.0.0/0"
 }
 
 variable "aws_region" {
   type    = string
   default = "ap-southeast-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "prj"
+}
+
+variable "project_env" {
+  type    = string
+  default = "dev"
 }
