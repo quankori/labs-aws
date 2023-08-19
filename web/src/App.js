d9ed2c74f8c643bd7b490791a0ed83b4fa75b398
k0r1 <mimnhquan.k0r1@gmail.com>
2023-08-19 20:18:17 +0700

    Update cicd

--- web/src/App.js
+++ web/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. test CI/CD
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello, Kori
+          Hello, Kori 
         </a>
       </header>
     </div>
